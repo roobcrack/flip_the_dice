a6e85a0e21bb26c7964c8500f3b10c5239e86c9f
roobcrack <39882967+roobcrack@users.noreply.github.com>
2024-04-17 13:17:48 +0200

    Add

--- program/src/Main.java
+++ program/src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Dice dice = new Dice();
 
+        //Insert number of players
+        int numPlayers=0;
+        do {
+            try{
+                System.out.print("Insert number of players: ");
+                numPlayers = Integer.parseInt(sc.nextLine());
+                if(numPlayers<=0)
+                    throw new IllegalArgumentException("number must be greater than 0");
+            } catch(NumberFormatException e){
+                System.out.println("  ERROR: Please enter a valid number");
+            } catch(IllegalArgumentException e){
+                System.out.print("  ERROR: "+e.getMessage());
+            }
+        } while (numPlayers <= 0);
+
+        //Add each player
+        Player[] players = new Player[numPlayers];
+        for(int i=0; i<numPlayers; i++){
+            System.out.print(i+1+".Insert player: ");
+            players[i] = new Player(sc.nextLine());
+        }
     }
 }
--- program/src/Player.java
+++ program/src/Player.java
@@ -2,8 +2,11 @@ public class Player {
     private String name;
     private int score;
 
-    public Player(String name, int score){
+    public Player(String name){
         this.name = name;
         this.score = 0;
+    }
+    public void increaseScore(){
+        score++;
     }
 }
